9d91b80b6fbe659c6b719b55b8e0bb213a85d9b0
Roxanne Franco <roxannefranco10@gmail.com>
2023-01-24 18:53:47 +0100

    Fixed login test

--- src/test/login.test.js
+++ src/test/login.test.js
@@ -2,9 +2,8 @@ import { login } from '../js/api/auth/login.js'
 
 describe('login', function () {
   it('stores a token when provided with valid credentials', async function () {
-    const data = await login('roxannetests@stud.noroff.no', '12345678')
-    // it is expected that data does not return accessToken
-    // because it is removed from profile object when a response is ok
-    expect(data).not.toHaveProperty('accessToken')
+    await login('roxannetests@stud.noroff.no', '12345678')
+    // it is expected that token is not null
+    expect(localStorage.getItem('token')).not.toBeNull()
   })
 })
