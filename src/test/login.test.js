f43d4086dec0e62ed6c81c09a61856fc0b44cb4c
Roxanne Franco <roxannefranco10@gmail.com>
2023-01-24 18:38:19 +0100

    Added comments

--- src/test/login.test.js
+++ src/test/login.test.js
@@ -2,7 +2,9 @@ import { login } from '../js/api/auth/login.js'
 
 describe('login', function () {
   it('stores a token when provided with valid credentials', async function () {
-    const promise = await login('roxannetests@stud.noroff.no', '12345678')
-    expect(promise).not.toHaveProperty('accessToken')
+    const data = await login('roxannetests@stud.noroff.no', '12345678')
+    // it is expected that data does not return accessToken
+    // because it is removed from profile object when a response is ok
+    expect(data).not.toHaveProperty('accessToken')
   })
 })
