da5fcf180f0155747bff61588f1c1b1cf0e07218
Roxanne Franco <roxannefranco10@gmail.com>
2023-01-26 21:26:20 +0100

    E2e test for login

---
+++ cypress/e2e/3-testing/login.cy.js
@@ -0,0 +1,12 @@
+describe('Test login', () => {
+  it('can log in with the login form with valid credentials', () => {
+    cy.visit('/')
+    cy.wait(1000)
+    cy.get('[data-bs-target="#loginModal"]').first().click({ force: true })
+    cy.wait(500)
+    cy.get('#loginEmail').type('roxannetests@stud.noroff.no')
+    cy.get('#loginPassword').type('12345678')
+    cy.get('#loginForm button.btn-success').first().click({ force: true })
+    cy.location('search').should('eq', '?view=profile&name=roxanne_franco')
+  })
+})
